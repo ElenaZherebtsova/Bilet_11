ae5982e644353d8906ce020ff558d0e6330a458b
Elena <lenulka.90@mail.ru>
2024-03-01 17:33:14 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> first = new ArrayList<>(List.of("odin", "dva", "dva", "tri"));
+
+        List <String> second = first.stream()
+                .distinct().collect(Collectors.toList());
+
+        for (String i :
+                second) {
+            System.out.println(i);
+        }
+
+
+    }
+}
