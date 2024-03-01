5ad3e349dc9b56d601fe6d6ef7a18bed9a651cef
Elena <lenulka.90@mail.ru>
2024-03-01 17:35:50 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
         List<String> first = new ArrayList<>(List.of("odin", "dva", "dva", "tri"));
 
         List <String> second = first.stream()
@@ -16,7 +15,5 @@ public class Main {
                 second) {
             System.out.println(i);
         }
-
-
     }
 }
